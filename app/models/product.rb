5d12764adb25caf5da4b8c6cfdec9fab3b3fc0b9
Samantha Morco <samanthamorco@gmail.com>
2017-01-24 23:03:15 -0500

    demo adding model methods to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ActiveRecord::Base
+
+  def sale_message
+    message = "Discount item!!!" if price.to_i <= 5
+    message = "Every day value!!" if price.to_i > 5
+    message
+  end
+
+  def tax
+    price.to_i * 0.09
+  end
+
+  def total
+    price.to_i + tax
+  end
+
+  def red_price
+    "red-price" if sale_message == "Discount item!!!"
+  end
+
 end
