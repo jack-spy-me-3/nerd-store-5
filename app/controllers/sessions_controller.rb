49d9567be39e33faae8ba3abf6041f7ba01c204c
Samantha Morco <samanthamorco@gmail.com>
2017-02-19 14:43:04 -0500

    seed file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Logged in!!"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-Product.all.each do |product|
-  Image.create(url: product.image, product_id: product.id)
-end
+# Product.all.each do |product|
+#   Image.create(url: product.image, product_id: product.id)
+# end
+User.create
+Group.create
+Subgroup.create()
