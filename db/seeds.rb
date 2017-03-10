d5cd395e120cc484871331d540f71c0afc12e593
Samantha Morco <samanthamorco@gmail.com>
2017-03-09 19:21:33 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,13 @@ User.create!([
   {name: nil, email: "dkfshdfkdsjhfskldjfhsdlkjfhsdlkf", password: "password", password_confirmation: "password", admin: false},
   {name: nil, email: "blahblah@gmail.com", password: "password", password_confirmation: "password", admin: false}
 ])
+Supplier.create!([
+  {name: "Think Geek", email: "think@geek.com", phone: "123-132-1234"},
+  {name: "Amazon", email: "ama@zon.com", phone: "555-555-5555"},
+  {name: "Hot Topic", email: "hot@topic.com", phone: "123-123-1234"}
+])
 Product.create!([
-  {name: "Hitchhiker's Guide to the Galexy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.", stock: true, supplier_id: 1},
+  {name: "Hitchhiker's Guide to the Galexy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom..", stock: true, supplier_id: 1},
   {name: "DnD Dice set", price: "57.0", description: "Take down mighty dragons with this timeless set of 20 sided wonders", stock: true, supplier_id: 1},
   {name: "Space Cowboy Laser Gun", price: "170.0", description: "This weapon has no other description than, Shiney!", stock: true, supplier_id: 1},
   {name: "WNYX Mug", price: "200.0", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape", stock: true, supplier_id: 2},
@@ -71,8 +76,4 @@ ProductCategory.create!([
   {product_id: 7, category_id: 3},
   {product_id: 7, category_id: 5}
 ])
-Supplier.create!([
-  {name: "Think Geek", email: "think@geek.com", phone: "123-132-1234"},
-  {name: "Amazon", email: "ama@zon.com", phone: "555-555-5555"},
-  {name: "Hot Topic", email: "hot@topic.com", phone: "123-123-1234"}
-])
+
